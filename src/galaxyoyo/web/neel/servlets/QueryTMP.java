c0633714780987667be09c00b200eb0ada07dad2
galaxyoyo <yohann.danello@gmail.com>
2015-06-15 21:59:13 +0200

    La page s'affiche désormais en HTML 5

--- src/galaxyoyo/web/neel/servlets/QueryTMP.java
+++ src/galaxyoyo/web/neel/servlets/QueryTMP.java
@@ -16,23 +16,27 @@ public class QueryTMP extends HttpServlet
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
 	{
-		resp.setContentType("text/plain");
+		resp.setContentType("text/html; charset=UTF-8");
 		Query query = MCServerUtil.query("212.129.49.101", 40242);
-		resp.getWriter().write("IP : " + query.getHostIP() + ":" + query.getHostPort() + "\n");
-		resp.getWriter().write("Message du jour : " + query.getHostname() + "\n");
-		resp.getWriter().write("Jeu : " + query.getGameId() + "\n");
-		resp.getWriter().write("Type de jeu : " + query.getGametype() + "\n");
-		resp.getWriter().write("Version du jeu : " + query.getVersion() + "\n");
-		resp.getWriter().write("Serveur : " + query.getMotor() + "\n");
-		resp.getWriter().write("Plugins : " + query.getPlugins() + "\n");
-		resp.getWriter().write("Dossier de la map : " + query.getMap() + "\n");
+		resp.getWriter().write("<!doctype html>\r\n<head>\r\n<meta http-equiv=\"content-type\" "
+						+ "content=\"text/html; charset=UTF-8\" />\r\n<title>Query Serveur</title>\r\n"
+						+ "</head>\r\n<body>\r\n");
+		resp.getWriter().write("IP : " + query.getHostIP() + ":" + query.getHostPort() + "<br />\r\n");
+		resp.getWriter().write("Message du jour : " + query.getHostname() + "<br />\r\n");
+		resp.getWriter().write("Jeu : " + query.getGameId() + "<br />\r\n");
+		resp.getWriter().write("Type de jeu : " + query.getGametype() + "<br />\r\n");
+		resp.getWriter().write("Version du jeu : " + query.getVersion() + "<br />\r\n");
+		resp.getWriter().write("Serveur : " + query.getMotor() + "<br />\r\n");
+		resp.getWriter().write("Plugins : " + query.getPlugins() + "<br />\r\n");
+		resp.getWriter().write("Dossier de la map : " + query.getMap() + "<br />\r\n");
 		resp.getWriter().write(query.getConnectedPlayersNumber() + " / " + query.getMaxPlayers()
 						+ " joueur" + (query.getConnectedPlayersNumber() > 1 ? "s" : "") + " connect\u00e9"
 						+ (query.getConnectedPlayersNumber() > 1 ? "s" : "")
-						+ (query.getConnectedPlayersNumber() > 0 ? " :" : "") + "\n");
+						+ (query.getConnectedPlayersNumber() > 0 ? " :" : "") + "<br />\r\n");
 		for (String player : query.getConnectedPlayers())
 		{
-			resp.getWriter().write("\t- " + player + "\n");
+			resp.getWriter().write(" &nbsp; &nbsp; &nbsp; &nbsp;- " + player + "\n");
 		}
+		resp.getWriter().write("</body>\r\n</html>");
 	}
 }
